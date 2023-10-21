5984fef4c2c93eaebc85f2d2669af18934e41a77
Christian Pompeu <ochristianpompeu@gmail.com>
2023-10-21 11:19:47 -0300

    iniciada estrutura de pastas da nova versao do next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
+'use client'
+import SignInSide from '@/components/SignInSide'
 import { Container } from '@mui/material'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <Container maxWidth="sm" className='h-full w-full justify-center justify-items-center'>
-      <h2>
-        Hello
-      </h2>
-    </Container>
+    <SignInSide />
   )
 }
