a50a4c0eabfd0ca54b9ac0b88cb375922e8abc31
Christian <114309489+ochristianpompeu@users.noreply.github.com>
2023-10-21 14:58:16 -0300

    configurando chakra para next js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 'use client'
 import SignInSide from '@/components/SignInSide'
-import { Container } from '@mui/material'
-import Image from 'next/image'
 
 export default function Home() {
   return (
