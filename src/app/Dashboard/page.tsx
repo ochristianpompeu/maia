51bba9798fe65858a43a5d040e102bb563173ba4
Christian Pompeu <ochristianpompeu@gmail.com>
2023-10-24 13:39:55 -0300

    Iniciando crud de organizacao
    Criando no sidebar do dashboard

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -1,9 +1,13 @@
+import NavBar from "@/components/NavBar/NavBar";
+import Sidebar from "@/components/SideBar/Sidebar";
 import { Button, Container, Heading } from "@chakra-ui/react";
 // import {  } from "next-auth"
 
 export default function Dashboard() {
   return (
-    <Container margin={0} p={0}>
+    <Container margin={0} p={0} maxW="full">
+      <NavBar />
+      <Sidebar />
     </Container>
   );
 }
