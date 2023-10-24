b7b286e02ab37d69386dae066cd3b844c9b447a3
Christian Pompeu <ochristianpompeu@gmail.com>
2023-10-24 16:23:51 -0300

    Atualizando index
    Ajustando dimensões máximas da tela inicial

--- src/app/SplitScreen/SplitScreen.tsx
+++ src/app/SplitScreen/SplitScreen.tsx
@@ -1,24 +1,26 @@
 "use client";
 
 import { Container, Stack } from "@chakra-ui/react";
-import LoginSide from "./LoginSide";
-import ImageSide from "./ImageSide";
 import NavBar from "../../components/NavBar/NavBar";
+import ImageSide from "./ImageSide";
+import LoginSide from "./LoginSide";
 
 export default function SplitScreen() {
   return (
     <Container maxW="full" p={0}>
       <NavBar />
-      <Stack
-        m={0}
-        p={0}
-        minH={"100vh"}
-        direction={{ base: "column-reverse", md: "row" }}
-        spacing={0}
-      >
-        <LoginSide />
-        <ImageSide />
-      </Stack>
+      <Container p={0} maxW="container.xl">
+        <Stack
+          m={0}
+          p={0}
+          minH={"100vh"}
+          direction={{ base: "column-reverse", md: "row" }}
+          spacing={0}
+        >
+          <LoginSide />
+          <ImageSide />
+        </Stack>
+      </Container>
     </Container>
   );
 }
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,6 +3,7 @@
 import { CloseIcon, HamburgerIcon } from "@chakra-ui/icons";
 import {
   Box,
+  Container,
   Flex,
   HStack,
   IconButton,
@@ -19,34 +20,40 @@ export default function NavBar() {
 
   return (
     <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
-      <Flex h={12} alignItems={"center"} justifyContent={"space-between"}>
-        <IconButton
-          size={"md"}
-          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-          aria-label={"Open Menu"}
-          display={{ md: "none" }}
-          onClick={isOpen ? onClose : onOpen}
-        />
-        <HStack spacing={8} alignItems={"center"}>
-          <Box>Logo</Box>
-          <HStack as={"nav"} spacing={4} display={{ base: "none", md: "flex" }}>
-            {Links.map((link) => (
-              <NavLink key={link}>{link}</NavLink>
-            ))}
+      <Container maxW="container.xl" p={0}>
+        <Flex h={12} alignItems={"center"} justifyContent={"space-between"}>
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={"center"}>
+            <Box>Logo</Box>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
           </HStack>
-        </HStack>
-        <AvatarComMenu />
-      </Flex>
+          <AvatarComMenu />
+        </Flex>
 
-      {isOpen ? (
-        <Box pb={4} display={{ md: "none" }}>
-          <Stack as={"nav"} spacing={4}>
-            {Links.map((link) => (
-              <NavLink key={link}>{link}</NavLink>
-            ))}
-          </Stack>
-        </Box>
-      ) : null}
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Container>
     </Box>
   );
 }
