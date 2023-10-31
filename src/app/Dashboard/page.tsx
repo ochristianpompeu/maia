6ec7a29903e050fab1faebab1ef596e42c978abd
Christian <114309489+ochristianpompeu@users.noreply.github.com>
2023-10-31 10:33:31 -0300

    Fiz dashboard folder name

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -1,6 +1,5 @@
 import SidebarDashboard from "@/components/SideBarDashboard/SidebarDashboard";
 import { Container } from "@chakra-ui/react";
-// import {  } from "next-auth"
 
 export default function Dashboard() {
   return (
