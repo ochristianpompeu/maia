5c00cb4c1a1ba9ffeb1350eadebdb7bb865c82fd
Christian <114309489+ochristianpompeu@users.noreply.github.com>
2023-11-13 12:54:31 -0300

    Model Professional

--- src/models/professional.ts
+++ src/models/professional.ts
@@ -1,10 +1,37 @@
-import { Schema } from "mongoose";
+import { ObjectId } from "mongodb";
+import mongoose, { Schema, models } from "mongoose";
 
 const professionalSchema = new Schema(
-    {
-        name:{
-            type: String,
-            required: true,
-        }
-    }
-)
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    orgId: {
+      type: ObjectId,
+      required: false,
+    },
+    bio: { type: String, required: false },
+    email: {
+      type: String,
+      required: false,
+    },
+    function: {
+      type: String,
+      required: false,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+    services: [{ type: ObjectId, required: false, ref: "Service" }],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Professional =
+  models.Professional || mongoose.model("Professional", professionalSchema);
+export { Professional };
+
