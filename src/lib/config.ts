233722bbed803985459a3b6e8c790cd9099dfd0e
Christian <114309489+ochristianpompeu@users.noreply.github.com>
2023-11-13 10:17:20 -0300

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 export const applicationConfig = {
   // baseUrl: "http://localhost:3000",
   // baseUrl: "http://127.0.0.1:3000",
-  // baseUrl: "https://maia-two.vercel.app",
-  baseUrl: "https://maia-app.vercel.app",
+  baseUrl: "https://maia-two.vercel.app",
+  // baseUrl: "https://maia-app.vercel.app",
 };
