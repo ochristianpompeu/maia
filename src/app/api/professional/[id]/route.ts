8a012bbc44724ff3b2e4f2a18ff8051bd16133dc
Christian <114309489+ochristianpompeu@users.noreply.github.com>
2023-11-16 20:12:03 -0300

    professional update concluded

--- src/app/api/professional/[id]/route.ts
+++ src/app/api/professional/[id]/route.ts
@@ -2,27 +2,40 @@ import { connectMongoDB } from "@/lib/mongodb";
 import { Professional } from "@/models/professional";
 import { NextResponse } from "next/server";
 
-// export async function PUT(
-//   request: Request,
-//   { params }: { params: { id: string } }
-// ) {
-//   const { id } = params;
-//   const { newName: name, newDescription: description } = await request.json();
-//   await connectMongoDB();
+export async function PUT(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  const {
+    newName: name,
+    newEmail: email,
+    newImage: image,
+    newBio: bio,
+    newFunc: func,
+    newOrgId: orgId,
+    newServices: services,
+  } = await request.json();
+  await connectMongoDB();
 
-//   await Organization.findByIdAndUpdate(id, {
-//     name: name,
-//     description: description,
-//   });
-//   return NextResponse.json(
-//     {
-//       message: "Organização atualizada",
-//     },
-//     {
-//       status: 200,
-//     }
-//   );
-// }
+  await Professional.findByIdAndUpdate(id, {
+    name: name,
+    orgId: orgId,
+    bio: bio,
+    email: email,
+    function: func,
+    image: image,
+    services: services,
+  });
+  return NextResponse.json(
+    {
+      message: "Profissional atualizado",
+    },
+    {
+      status: 200,
+    }
+  );
+}
 
 export async function GET(
   request: Request,
