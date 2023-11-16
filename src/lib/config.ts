268fd6aa404c3ea1b8d6efcdaf14d5b3b762a47b
Christian <114309489+ochristianpompeu@users.noreply.github.com>
2023-11-16 14:45:21 -0300

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,8 +2,8 @@ export const applicationConfig = {
   staticHeight: "calc(100vh - 48px - 64px)",
   staticHeightPanel: "calc(100vh - 56px - 64px)",
   staticHeightPanelCard: "calc(100vh - 64px - 64px)",
-  baseUrl: "http://localhost:3000",
+  // baseUrl: "http://localhost:3000",
   // baseUrl: "http://127.0.0.1:3000",
-  // baseUrl: "https://maia-two.vercel.app",
+  baseUrl: "https://maia-two.vercel.app",
   // baseUrl: "https://maia-app.vercel.app",
 };
